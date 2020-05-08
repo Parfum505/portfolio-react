e8c669d1e90f241a03633f5be7464f0d9b545d2e
Pavel Kryvytskyi <pavel.kryvytskyi@gmail.com>
2020-05-08 18:46:21 +0200

    add animation, media to Home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { faFacebookSquare, faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import LinkIcon from "./LinkIcon/LinkIcon";
 
@@ -8,10 +8,51 @@ const Home = () => {
         {name: faLinkedin, link: "https://www.linkedin.com/in/pavel-kryvytskyi-370720124/"},
         {name: faGithubSquare, link: "https://github.com/Parfum505?tab=repositories"}
     ];
+    const title = ("Hi,I'm Pavel Kryvytskyi").split("");
+    useEffect(() => {
+        const title = document.querySelector("#home .lg-heading");
+        const subTitle = document.querySelector("#home .sm-heading");
+        title.classList.add('loaded');
+        const letters = Array.prototype.slice.call(document.querySelectorAll("#home .letter"));
+        const lastLetterIndex = letters.length - 1;
+
+        letters.forEach((item, idx) => {
+            item.addEventListener('animationend', () => {
+                item.style.opacity = 1;
+                if (idx === lastLetterIndex) {
+                    title.classList.remove('loaded');
+                    subTitle.style.opacity = 1;
+                }
+            });
+        });
+        return () => {
+            letters.forEach((item, idx) => {
+                item.removeEventListener('animationend', () => {
+                    item.style.opacity = 1;
+                    if (idx === lastLetterIndex) {
+                        title.classList.remove('loaded');
+                        subTitle.style.opacity = 1;
+                    }
+                });
+            });
+        }
+    });
     return (
         <main id="home">
             <h1 className="lg-heading">
-                Hi,<br />I'm <span className="text-secondary">Pavel</span>
+                {
+                    title.map((item, i) => {
+                        if(item === ",") {
+                            return (<span key={i} className="letter">{item}</span>,<br  key="br" />)
+                        }else if(item === " ") {
+                            return <span key={i} >{item}</span>
+                        } else if(i < 7) {
+                            return <span key={i} className="letter">{item}</span>
+                        }  else if(item){
+                            return <span key={i} className="letter text-secondary">{item}</span>
+                        }
+                    })
+                }
             </h1>
             <h2 className="sm-heading">Front End Developer / Software Engineer</h2>
             <div className="icons">
